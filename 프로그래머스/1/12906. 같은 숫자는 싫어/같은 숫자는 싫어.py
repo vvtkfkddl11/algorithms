59c15038a586a8d74b3833b34f0539bf2a9fd456
Sarang Kim <77392642+vvtkfkddl11@users.noreply.github.com>
2024-07-09 10:34:42 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 120.06 ms, Memory: 27.8 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -1,21 +1,12 @@
 def solution(arr):
     answer = []
     # 스택 or 큐 사용
-    # i = 1
-    # while i < len(arr):
-    #     if arr[i] == arr[i-1]:
-    #         arr.pop(i)
-    #     else:
-    #         answer.append(arr[i-1])
-    #         i += 1
-    # answer.append(arr[-1])
-    answer = []
-    for i in range(len(arr)):
-        # 첫 번째 요소는 무조건 추가
-        if i == 0:
-            answer.append(arr[i])
-        # 현재 요소가 이전 요소와 다를 때만 추가
-        elif arr[i] != arr[i - 1]:
-            answer.append(arr[i])
-    return answer
+    i = 1
+    while i < len(arr):
+        if arr[i] == arr[i-1]:
+            arr.pop(i)
+        else:
+            i += 1
+        
+    answer = arr
     return answer
