44027c0015026b276046d730351a12f60721ef0d
Sarang Kim <77392642+vvtkfkddl11@users.noreply.github.com>
2024-07-21 11:23:09 +0900

    [level 1] Title: 신고 결과 받기, Time: 229.81 ms, Memory: 45.5 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.py
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.py
@@ -5,7 +5,7 @@ def solution(id_list, report, k):
     answer = []
     report = list(set(report))  # 중복 신고 제거
     dic_report = defaultdict(set)  # 신고자 - 신고 받은 사람 (report와 같은 set형으로 맞춰줌)
-    dic_count = {}  # 신고 받은 사람 - 신고 받은 횟수
+    dic_count = {}  # 신고 받은 사람 - 신고 받은 횟수 / defaultdict(set)
 
     for i in id_list:
         dic_count[i] = 0
