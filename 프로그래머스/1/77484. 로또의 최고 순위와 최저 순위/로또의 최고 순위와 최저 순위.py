3d748b9f73e850ce99a606614a5a77adf3983d7c
Sarang Kim <77392642+vvtkfkddl11@users.noreply.github.com>
2024-07-22 23:29:31 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.py
@@ -0,0 +1,34 @@
+def solution(lottos, win_nums):
+    answer = []
+    rank = {1: 6, 2: 5, 3: 4, 4: 3, 5: 2}  # 순위, 당첨 일치 개수
+    count = 0
+    count_zero = 0
+    max_rank = 0
+    min_rank = 0
+
+
+    for i in lottos:
+        if i in win_nums:
+            count += 1
+        if i == 0:
+            count_zero += 1
+
+    for a, b in rank.items():
+        if b == count+count_zero:
+            max_rank = a
+
+    for a, b in rank.items():
+        if b == count:
+            min_rank = a
+            
+    if count < 2:
+        print("hi")
+        min_rank = 6
+    
+    if count+count_zero < 2:
+        max_rank = 6
+        
+    answer.append(max_rank)
+    answer.append(min_rank)
+    
+    return answer
