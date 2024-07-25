075dcb14c97423f95346bdb46b903787f1dcb58d
Sarang Kim <77392642+vvtkfkddl11@users.noreply.github.com>
2024-07-25 11:14:27 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 12.77 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/README.md
+++ 프로그래머스/2/1844. 게임 맵 최단거리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 게임 맵 최단거리 - 1844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 12.79 ms
+메모리: 10.4 MB, 시간: 12.77 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 25일 11:12:37
+2024년 07월 25일 11:14:24
 
 ### 문제 설명
 
--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,33 +1,28 @@
-# 최단 거리 -> bfs
-
-from collections import deque 
+from collections import deque
 
 def solution(maps):
-    answer = 0
+    
     n = len(maps)
     m = len(maps[0])
-    dx = [0, 0, 1, -1]
-    dy = [1, -1, 0, 0]
     visited = [[0 for _ in range(m)] for _ in range(n)]
     visited[0][0] = 1
-    # visited = [0] * len(maps) for _ in len(maps[0])
+    dx = [0, 0, 1, -1]
+    dy = [1, -1, 0, 0]
     queue = deque()
     queue.append((0, 0))
-    
+
     while queue:
         a, b = queue.popleft()
-        # if 도달
-        if a == n-1 and b == m-1:
+        # 끝 점에 도달하면 이동 횟수를 출력
+        if a == n - 1 and b == m - 1:
             return visited[a][b]
         for i in range(4):
             nx = a + dx[i]
             ny = b + dy[i]
-        # 벽이면
             if nx < 0 or nx >= n or ny < 0 or ny >= m:
                 continue
+            # 다음 이동할 곳이 벽이 아니고, 아직 한 번도 방문하지 않은 곳이면
             elif maps[nx][ny] == 1 and visited[nx][ny] == 0:
-                # visited[nx][ny] == 1
                 visited[nx][ny] = visited[a][b] + 1
                 queue.append((nx, ny))
-            
     return -1
