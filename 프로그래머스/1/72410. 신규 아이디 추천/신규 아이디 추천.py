65c8eebd80e7b85b21834bc8d8dc5b6146201886
Sarang Kim <77392642+vvtkfkddl11@users.noreply.github.com>
2024-07-26 02:00:28 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.32 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.py
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.py
@@ -1,41 +1,36 @@
 def solution(new_id):
     answer = ''
+    
+    # 1단계
     new_id = new_id.lower()
-    # print(new_id)  # 1단계
 
-    second = ''
-    third = ''
-    fourth = ''
-
+    # 2단계
     for str in new_id:
         if str.isalpha() or str.isdigit() or str == '-' or str == '_' or str == '.':
             answer += str
-    # print(second)  # 2단계
 
+    # 3단계
     while '..' in answer:
         answer = answer.replace('..', '.')
-    # print(third)  # 3단계
     
+    # 4단계
     if len(answer) > 1:
         if answer[0] == '.':
             answer = answer[1:]
-        else: 
-            '.'
-    
     if answer[-1] == '.':
         answer = answer[:-1]
-    print(answer)  # 4단계
 
+    # 5단계
     if answer == '':
         answer = "a"
-    # print(third)  # 5단계
 
+    # 6단계
     if len(answer) >= 16:
         answer = answer[:15]
         if answer[-1] == '.':
             answer = answer[:-1]
-    # print(third)  # 6단계
-
+    
+    # 7단계
     while len(answer) <= 2:
         answer += answer[-1]
 
