8cf2efca545e88c96317719784f3f20627132716
Sarang Kim <77392642+vvtkfkddl11@users.noreply.github.com>
2024-07-27 21:50:05 +0900

    [level 1] Title: 시저 암호, Time: 2.13 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/README.md
+++ 프로그래머스/1/12926. 시저 암호/README.md
@@ -1,10 +1,10 @@
 # [level 1] 시저 암호 - 12926 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12926?language=python3#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12926) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 3.48 ms
+메모리: 10.2 MB, 시간: 2.13 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 27일 21:34:37
+2024년 07월 27일 21:50:02
 
 ### 문제 설명
 
--- 프로그래머스/1/12926. 시저 암호/시저 암호.py
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.py
@@ -1,23 +1,33 @@
 def solution(s, n):
-    answer = ''
+    s = list(s)
+    for i in range(len(s)):
+        if s[i].isupper():
+            s[i]=chr((ord(s[i])-ord('A')+ n)%26+ord('A'))
+        elif s[i].islower():
+            s[i]=chr((ord(s[i])-ord('a')+ n)%26+ord('a'))
+            
+    return "".join(s)
 
-    for i in s:
-        if i == " ":
-            answer += " "
-            continue
-        if 65 <= ord(i) <= 90:
-            if ord(i)+n > 90:
-                answer += chr((ord(i) - 65 + n) % 26 + 65)
-            else: 
-                answer += chr(ord(i)+n)
-                print(i)
-                print(ord(i))
-        else:
-            if ord(i)+n > 122:
-                answer += chr((ord(i) - 97 + n) % 26 + 97)
-            else:
-                answer += chr(ord(i)+n)
+# def solution(s, n):
+#     answer = ''
+
+#     for i in s:
+#         if i == " ":
+#             answer += " "
+#             continue
+#         if 65 <= ord(i) <= 90:
+#             if ord(i)+n > 90:
+#                 answer += chr((ord(i) - 65 + n) % 26 + 65)
+#             else: 
+#                 answer += chr(ord(i)+n)
+#                 print(i)
+#                 print(ord(i))
+#         else:
+#             if ord(i)+n > 122:
+#                 answer += chr((ord(i) - 97 + n) % 26 + 97)
+#             else:
+#                 answer += chr(ord(i)+n)
         
-    print(answer)
+#     print(answer)
     
-    return answer
+#     return answer
