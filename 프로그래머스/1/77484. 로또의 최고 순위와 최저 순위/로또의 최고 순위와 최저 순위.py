c1a507d46302d3fc0cdd6fbd7d0de3d724903721
Sarang Kim <77392642+vvtkfkddl11@users.noreply.github.com>
2024-07-28 10:55:54 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.py
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.py
@@ -1,33 +1,10 @@
 def solution(lottos, win_nums):
-    answer = []
-    rank = {1: 6, 2: 5, 3: 4, 4: 3, 5: 2}  # 순위, 당첨 일치 개수
-    count_win = 0
-    count_zero = 0
-    max_rank = 0
-    min_rank = 0
+    rank = [6, 6, 5, 4, 3, 2, 1]  # 인덱스 0부터 6까지의 순위 (일치하는 숫자 개수에 따른 순위)
 
-    for i in lottos:
-        if i in win_nums:
-            count_win += 1
-        if i == 0:
-            count_zero += 1
+    count_win = sum(1 for i in lottos if i in win_nums)
+    count_zero = sum(1 for i in lottos if i == 0)
 
-    for a, b in rank.items():
-        if b == count_win+count_zero:
-            max_rank = a
+    max_rank = rank[count_win + count_zero]
+    min_rank = rank[count_win]
 
-    for a, b in rank.items():
-        if b == count_win:
-            min_rank = a
-            
-    if count_win < 2:
-        min_rank = 6
-    
-    # elif로 해줘서 14번 테케 틀렸음
-    if count_win+count_zero < 2:
-        max_rank = 6
-        
-    answer.append(max_rank)
-    answer.append(min_rank)
-    
-    return answer
+    return [max_rank, min_rank]
