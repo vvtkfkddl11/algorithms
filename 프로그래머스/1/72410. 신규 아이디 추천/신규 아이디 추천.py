cf207cd7d200d52ef0944f35345167e30d050442
Sarang Kim <77392642+vvtkfkddl11@users.noreply.github.com>
2024-07-29 23:56:15 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.45 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.py
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.py
@@ -1,37 +1,95 @@
 def solution(new_id):
     answer = ''
-    
-    # 1단계
+
+    # level 1
     new_id = new_id.lower()
+    print(new_id)
 
-    # 2단계
-    for str in new_id:
-        if str.isalpha() or str.isdigit() or str == '-' or str == '_' or str == '.':
-            answer += str
+    # level 2
+    for i in new_id:
+        if i.isalpha() or i.isdigit() or i == '-' or i == '_' or i == '.':
+            answer += i
+    print(answer)
 
-    # 3단계
+    # level 3
     while '..' in answer:
         answer = answer.replace('..', '.')
-    
-    # 4단계
+
+    print("l3: ", answer)
+
+    # level 4
     if len(answer) > 1:
         if answer[0] == '.':
             answer = answer[1:]
     if answer[-1] == '.':
         answer = answer[:-1]
 
-    # 5단계
-    if answer == '':
-        answer = "a"
+    # level 5
+    if len(answer) == 0:
+        answer += 'a'
 
-    # 6단계
+    # level 6
     if len(answer) >= 16:
         answer = answer[:15]
         if answer[-1] == '.':
             answer = answer[:-1]
-    
-    # 7단계
+
+    # level 7
     while len(answer) <= 2:
         answer += answer[-1]
 
     return answer
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def solution(new_id):
+#     answer = ''
+    
+#     # 1단계
+#     new_id = new_id.lower()
+
+#     # 2단계
+#     for str in new_id:
+#         if str.isalpha() or str.isdigit() or str == '-' or str == '_' or str == '.':
+#             answer += str
+
+#     # 3단계
+#     while '..' in answer:
+#         answer = answer.replace('..', '.')
+    
+#     # 4단계
+#     if len(answer) > 1:
+#         if answer[0] == '.':
+#             answer = answer[1:]
+#     if answer[-1] == '.':
+#         answer = answer[:-1]
+
+#     # 5단계
+#     if answer == '':
+#         answer = "a"
+
+#     # 6단계
+#     if len(answer) >= 16:
+#         answer = answer[:15]
+#         if answer[-1] == '.':
+#             answer = answer[:-1]
+    
+#     # 7단계
+#     while len(answer) <= 2:
+#         answer += answer[-1]
+
+#     return answer
