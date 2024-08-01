35782b162077f181d32c4505c05fbc3197bd4263
Sarang Kim <77392642+vvtkfkddl11@users.noreply.github.com>
2024-08-01 23:44:54 +0900

    [level 2] Title: 전화번호 목록, Time: 456.48 ms, Memory: 46.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -0,0 +1,15 @@
+def solution(phone_book):
+    answer = True
+    hashMap = {}
+    
+    for i in phone_book:
+        hashMap[i] = ''
+    
+    for number in phone_book:
+        temp = ''
+        for digit in number:
+            temp += digit
+            if temp in hashMap and temp != number:
+                return False
+            
+    return True
