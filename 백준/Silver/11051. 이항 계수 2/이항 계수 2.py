2f1672676f9c187bc9e2e3a75beaa30c2f054ab3
Sarang Kim <77392642+vvtkfkddl11@users.noreply.github.com>
2024-08-23 18:14:00 +0900

    [Silver II] Title: 이항 계수 2, Time: 228 ms, Memory: 88152 KB -BaekjoonHub

---
+++ 백준/Silver/11051. 이항 계수 2/이항 계수 2.py
@@ -0,0 +1,16 @@
+import sys
+
+n, k = map(int, sys.stdin.readline().split())
+dp = [[0 for j in range(n+1)] for i in range(n+1)]
+
+for i in range(0, n+1):
+    dp[i][1] = i  # 5C1
+    dp[i][0] = 1  # 5C0
+    dp[i][i] = 1  # 5C1
+
+for i in range(0, n+1):
+    for j in range(1, i):
+        dp[i][j] = dp[i-1][j] + dp[i-1][j-1]
+
+num = dp[n][k]%10007
+print(num)
