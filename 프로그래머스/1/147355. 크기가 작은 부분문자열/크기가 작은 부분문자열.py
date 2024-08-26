02c97a87eec5fda561e2e56721df72ac312e99a5
Sarang Kim <77392642+vvtkfkddl11@users.noreply.github.com>
2024-08-26 19:50:39 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 7.32 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.py
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.py
@@ -1,10 +1,29 @@
+from collections import deque
+
 def solution(t, p):
     answer = 0
-    temp = ''
+    queue = deque()
+    p_len = len(p)
+    
     for i in str(t):
-        temp += i
-        if len(temp) == len(p):
+        queue.append(i)
+        if len(queue) == p_len:
+            # 현재 queue에 저장된 문자열을 정수로 변환하여 비교
+            temp = ''.join(queue)
             if int(temp) <= int(p):
                 answer += 1
-            temp = temp[1:]
+            # queue에서 첫 번째 문자를 제거
+            queue.popleft()
+    
     return answer
+
+# def solution(t, p):
+#     answer = 0
+#     temp = ''
+#     for i in str(t):
+#         temp += i
+#         if len(temp) == len(p):
+#             if int(temp) <= int(p):
+#                 answer += 1
+#             temp = temp[1:]
+#     return answer
