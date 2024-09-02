d7c349a3743593dcd9af4eaad129d76d0e78678a
Sarang Kim <77392642+vvtkfkddl11@users.noreply.github.com>
2024-09-02 13:14:04 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 4.45 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
@@ -1,32 +1,30 @@
 def solution(n, wires):
-    # answer = -1
+    answer = -1
     graph = [[] for _ in range(n+1)]
-    # visited = [[0] for _ in range(n+1)]
-    
-    for a, b in wires:
-        graph[a].append(b)
-        graph[b].append(a)
-        
-    def dfs(node):
-        cnt = 1
-        visited[node] = 1
-        for child in graph[node]:
-            if visited[child] == 0:
-                cnt += dfs(child) 
-        return cnt
-    
     min_v = float('inf')
     
     for a, b in wires:
-        visited = [0] * (n+1)
-        graph[a].remove(b)
-        graph[b].remove(a)
-        
-        cnta = dfs(a)
-        cntb = n - cnta
-        min_v = min(min_v, abs(cnta-cntb))
-        
         graph[a].append(b)
         graph[b].append(a)
+        
+    print(graph)
+    
+    def dfs(node):
+        cnt = 1
+        visited[node] = 1
+        for i in graph[node]:
+            if visited[i] == 0:
+                cnt += dfs(i)
+        return cnt
+    
+    for a, b in wires:
+        visited = [0]*(n+1)
+        graph[a].remove(b)
+        graph[b].remove(a)
+        cnta = dfs(a)
+        cntb = n - cnta
+        graph[a].append(b)
+        graph[b].append(a)
+        min_v = min(min_v, abs(cnta-cntb))
         
     return min_v
