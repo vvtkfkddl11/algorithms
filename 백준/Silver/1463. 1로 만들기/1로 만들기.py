cc1c3fe81830ebbad097b9a661b7913979c4bf05
Sarang Kim <77392642+vvtkfkddl11@users.noreply.github.com>
2025-03-26 06:12:25 +0900

    [Silver III] Title: 1로 만들기, Time: 248 ms, Memory: 40224 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -1,16 +1,23 @@
 import sys
 
 n = int(sys.stdin.readline())
-dp = [0]*(n+1)
-# dp[i]: i에서 1로 만드는 데 걸리는 최소 연산 횟수
 
-dp[1] = 0  # 1일 때 연산 불필요
 
-for i in range(2, n+1):
-    dp[i] = dp[i-1] + 1
-    if i % 2 == 0:
-        dp[i] = min(dp[i], dp[i//2] + 1)
-    if i % 3 == 0:
-        dp[i] = min(dp[i], dp[i//3] + 1)
+def make(n):
+    # DP 테이블 초기화
+    dp = [0] * (n + 1)
+    dp[1] = 0
 
-print(dp[n])
+    # 작은 문제부터 차례로 해결
+    for i in range(2, n + 1):
+        dp[i] = dp[i-1] + 1
+
+        if i % 2 == 0:
+            dp[i] = min(dp[i], dp[i//2]+1)
+        if i % 3 == 0:
+            dp[i] = min(dp[i], dp[i//3]+1)
+
+    return dp[n]
+
+
+print(make(n))
