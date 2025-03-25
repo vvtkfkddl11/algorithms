cc960df508d4df985186d269df70ed0e349fae1d
Sarang Kim <77392642+vvtkfkddl11@users.noreply.github.com>
2025-03-25 22:41:40 +0900

    [Silver III] Title: 달팽이, Time: 224 ms, Memory: 119308 KB -BaekjoonHub

--- 백준/Silver/1913. 달팽이/달팽이.py
+++ 백준/Silver/1913. 달팽이/달팽이.py
@@ -1,82 +1,47 @@
 import sys
 
-# 자연수 N 입력
 n = int(sys.stdin.readline())
+target = int(sys.stdin.readline())
 
-# 찾고자하는 자연수 입력
-find = int(sys.stdin.readline())
+graph = [[0]*n for _ in range(n)]
 
-# 결과 그래프
-graph = [[0]*(n) for _ in range(n)]
+# 하, 우, 상, 좌
+dx = [1, 0, -1, 0]
+dy = [0, 1, 0, -1]
+d = 0
+# d=0 (dx[0],dy[0]) 일때 하
+# d=1 (dx[1],dy[1]) 일때 우
+# d=2 (dx[2],dy[2]) 일때 상
+# d=3 (dx[3],dy[3]) 일때 좌
 
-# 상하좌우 이동 좌표
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
+x, y = 0, 0
+target_x, target_y = 0, 0
+num = n*n
 
-# 현재 시작 좌표 (중심점)
-x, y = n//2, n//2
-# 중심점에 번호 할당
-graph[x][y] = 1
+while num >= 1:
+    graph[x][y] = num
 
-# 시작 번호
-num = 2
-# 시작 외각 크기
-size = 3
+    if num == target:
+        target_x, target_y = x + 1, y + 1
 
-# 현재 좌표가 표를 다 돌아, 0, 0이 될때까지
-while (x != 0 or y != 0):
+    num -= 1
 
-    # 번호가 현재 외각의 크기만큼 할당될때까지
-    while (num <= size * size):
+    nx = x + dx[d]
+    ny = y + dy[d]
 
-        # 현재 좌표가 현재 외각의 시작 좌표라면, 상으로 이동
-        if (x == y == (n//2)):
-            # 상우하좌로 이동해야하는 횟수 저장
-            up, right, down, left = size, size-2, size-1, size-1
-            # 상으로 이동
-            x += dx[0]
-            y += dy[0]
-            # 상으로 이동해야하는 횟수 감소
-            up -= 1
+    # nx < 0 또는 nx >= n: x 좌표가 배열 범위를 벗어났는지 확인
+    # ny < 0 또는 ny >= n: y 좌표가 배열 범위를 벗어났는지 확인
+    # graph[nx][ny] != 0: 이미 숫자가 채워진 위치인지 확인
+    if nx < 0 or nx >= n or ny < 0 or ny >= n or graph[nx][ny] != 0:
+        d = (d+1) % 4
+        nx = x + dx[d]
+        ny = y + dy[d]
 
-        # 우하좌상 이동 횟수가 남아있다면 이동
-        elif (right > 0):
-            x += dx[3]
-            y += dy[3]
-            right -= 1
-        elif (down > 0):
-            x += dx[1]
-            y += dy[1]
-            down -= 1
-        elif (left > 0):
-            x += dx[2]
-            y += dy[2]
-            left -= 1
-        else:
-            x += dx[0]
-            y += dy[0]
-            up -= 1
+    x, y = nx, ny
 
-        # 현재 좌표에 번호 할당
-        graph[x][y] = num
-        num += 1
-
-    # 현재 외각에 대한 번호 할당이 끝났으므로, 
-    # 외각의 크기를 변경하고
-    size += 2
-    # 시작 좌표 변경을 위해 n 수정
-    n -= 2
-
-# 찾고자하는 값의 위치
-find_x, find_y = -1, -1
-
-# N개의 줄에 걸쳐 표를 출력
-for i in range(len(graph)):
-    for j in range(len(graph)):
-        if (graph[i][j] == find):
-            find_x, find_y = i, j
+for i in range(n):
+    for j in range(n):
         print(graph[i][j], end = " ")
-    print("")
+    print()
 
-# 찾고자하는 값의 위치 출력
-print(find_x + 1, find_y + 1)
+print(target_x, target_y)
