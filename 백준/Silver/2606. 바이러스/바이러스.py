4d5c1cb9976c22f1bc3a365c9df676798e30d4a7
Sarang Kim <77392642+vvtkfkddl11@users.noreply.github.com>
2025-03-26 03:37:29 +0900

    [Silver III] Title: 바이러스, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -4,25 +4,22 @@ answer = 0
 n = int(sys.stdin.readline())
 n_pairs = int(sys.stdin.readline())
 graph = [[] for i in range(n+1)]
-# graph = [[0]*2 for i in range(n_pairs)]
+visited = [0] * (n+1)
 
 for i in range(n_pairs):
     a, b = map(int, sys.stdin.readline().split())
     graph[a].append(b)
     graph[b].append(a)
 
-visited = []
-
 
 def dfs(node):
-    for connected_node in graph[node]:
-        if connected_node not in visited:
-            visited.append(connected_node)
-            dfs(connected_node)
+    visited[node] = 1
+    for nx in graph[node]:
+        if visited[nx] == 0:
+            dfs(nx)
 
 
-visited.append(1)
 dfs(1)
-
-answer = len(visited) - 1
+# print(visited)
+answer = sum(visited)-1
 print(answer)
