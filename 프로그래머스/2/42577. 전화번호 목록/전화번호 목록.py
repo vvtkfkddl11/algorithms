7f99d812ebab89e7c3683fc598061a52530151e7
Sarang Kim <77392642+vvtkfkddl11@users.noreply.github.com>
2025-03-26 21:32:44 +0900

    [level 2] Title: 전화번호 목록, Time: 442.22 ms, Memory: 45.7 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,15 +1,13 @@
 def solution(phone_book):
     answer = True
-    hashMap = {}
-    
+    dic = {}
+    for i in phone_book: 
+        dic[i] = 0
     for i in phone_book:
-        hashMap[i] = ''
-
-    for number in phone_book:
-        temp = ''
-        for digit in number:
-            temp += digit
-            if temp in hashMap and temp != number:
+        prefix = ""
+        for j in i:
+            prefix += j
+            if prefix in dic and i != prefix:
                 return False
-    
-    return True
+        
+    return answer
