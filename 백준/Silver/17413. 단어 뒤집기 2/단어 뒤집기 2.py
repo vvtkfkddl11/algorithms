fe08a1035cad635af757075cb4c860d362b7e52e
Sarang Kim <77392642+vvtkfkddl11@users.noreply.github.com>
2025-04-04 10:25:58 +0900

    [Silver III] Title: 단어 뒤집기 2, Time: 48 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.py
+++ 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.py
@@ -1,39 +1,35 @@
 import sys
 
-s = str(sys.stdin.readline().strip())
+s = sys.stdin.readline().strip()
 
-def solution(strs):
-    answer = ""
-    arr = []
-    flag = 0
-    reverse_strs = ""
-    tag_strs = ""
-
-    for i in range(len(strs)):
-        if strs[i] == ">":
-            tag_strs += strs[i]
-            arr.append(tag_strs)
-            tag_strs = ""
-            flag = 0
-        elif strs[i] == "<" or flag == 1:
-            if reverse_strs:
-                arr.append(reverse_strs)
-                reverse_strs = ""
-            flag = 1
-            tag_strs += strs[i]
-        elif strs[i] == " ":
-            reverse_strs += " "
-            arr.append(reverse_strs)
-            reverse_strs = ""
-        elif i == (len(strs)-1):
-            reverse_strs = (strs[i] + reverse_strs)
-            arr.append(reverse_strs)
+def solution(string):
+    result = ""
+    temp = ""
+    is_tag = False
+    
+    for char in string:
+        if char == '<':
+            # 태그가 시작되기 전 누적된 단어를 뒤집어서 결과에 추가
+            result += temp[::-1]
+            temp = '<'
+            is_tag = True
+        elif char == '>':
+            # 태그 닫기 문자를 포함하여 결과에 추가
+            result += temp + '>'
+            temp = ""
+            is_tag = False
+        elif char == ' ' and not is_tag:
+            # 태그 밖의 공백인 경우, 앞에 누적된 단어를 뒤집어서 추가하고 공백도 추가
+            result += temp[::-1] + ' '
+            temp = ""
         else:
-            reverse_strs = (strs[i] + reverse_strs)
-
-    for i in arr:
-        answer += i
-    return answer
-
+            # 문자 누적 (태그 내부 또는 단어)
+            temp += char
+    
+    # 남은 문자열 처리
+    if temp:
+        result += temp[::-1]
+    
+    return result
 
 print(solution(s))
