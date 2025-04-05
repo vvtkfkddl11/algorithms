ae13a2ad884af29e6d86f36d0586c1993d1f22be
Sarang Kim <77392642+vvtkfkddl11@users.noreply.github.com>
2025-04-05 22:26:53 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 256.00 ms, Memory: 27.1 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -1,23 +1,6 @@
-def solution(s):
-    a = []
-    for i in s:
-        if a[-1:] == [i]: continue
-        a.append(i)
-    return a
-
-
-# from collections import deque
-
-# def solution(arr):
-#     answer = []
-#     queue = deque(arr)
-#     x = queue.popleft()
-#     answer.append(x) 
-#     while queue:
-#         nx = queue.popleft()
-#         if x != nx:
-#             answer.append(nx) 
-#         if queue:
-#             x = nx
-            
-#     return answer
+def solution(arr):
+    answer = []
+    for i in arr:
+        if answer[-1:] != [i]:
+            answer.append(i)
+    return answer
