d7340254c6583851c553dd7084049cfac3fb7af9
Sarang Kim <77392642+vvtkfkddl11@users.noreply.github.com>
2025-04-10 13:35:23 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 1.48 ms, Memory: 9.32 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/README.md
+++ 프로그래머스/3/42627. 디스크 컨트롤러/README.md
@@ -1,10 +1,10 @@
 # [level 3] 디스크 컨트롤러 - 42627 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42627) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42627#) 
 
 ### 성능 요약
 
-메모리: 9.28 MB, 시간: 0.74 ms
+메모리: 9.32 MB, 시간: 1.48 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 10일 13:23:22
+2025년 04월 10일 13:35:21
 
 ### 문제 설명
 
--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
@@ -4,25 +4,25 @@ import heapq
 def solution(jobs):
     answer = 0
     jobs.sort(key=lambda x:x[0])
+    print(jobs)
     waiting = []
-    i = 0
-    completed = 0
-    current_time = 0
     total_time = 0
+    current_time = 0
+    completed = 0
+    i = 0
     
-    while completed < len(jobs):
-        while i < len(jobs) and current_time >= jobs[i][0]:
+    while completed <= len(jobs):
+        while i < len(jobs) and current_time <= jobs[i][0]:
             heapq.heappush(waiting, (jobs[i][1], jobs[i][0], i))
-            i+=1
-        
+            i += 1
         if waiting:
             duration, request_time, job_idx = heapq.heappop(waiting)
             current_time += duration
-            turnaround_time = (current_time - request_time)
-            total_time += turnaround_time
+            total_time = current_time - request_time
             completed += 1
         else:
             if i < len(jobs):
-                current_time = jobs[i][0]            
+                current_time = jobs[i][0]
+            
     
     return total_time // len(jobs)
