edcf5aba13cda5be467885332429f8f34b2a1c9c
Sarang Kim <77392642+vvtkfkddl11@users.noreply.github.com>
2025-04-10 17:26:58 +0900

    [level 2] Title: 기능개발, Time: 0.05 ms, Memory: 9.26 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/README.md
+++ 프로그래머스/2/42586. 기능개발/README.md
@@ -1,10 +1,10 @@
 # [level 2] 기능개발 - 42586 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586) 
 
 ### 성능 요약
 
-메모리: 9.25 MB, 시간: 0.10 ms
+메모리: 9.26 MB, 시간: 0.05 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 05일 14:43:09
+2025년 04월 10일 17:26:56
 
 ### 문제 설명
 
--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,24 +1,20 @@
 import math
-from collections import deque
+
 
 def solution(progresses, speeds):
     answer = []
-    temp = math.ceil((100 - progresses[0])/speeds[0])
     cnt = 0
-    visited = [0]*(len(progresses)) # 완료1/미완료0 상태를 담은 배열
-    while sum(visited)<len(visited):
-        for i in range(len(progresses)):
-            progresses[i] += temp*speeds[i]
-        for i in range(len(progresses)):
-            if progresses[i] >= 100 and visited[i] == 0:
-                cnt += 1
-                visited[i] = 1
-            if progresses[i] < 100:
-                temp = math.ceil((100 - progresses[i])/speeds[i])
-            if visited[i] == 0 and cnt > 0:
-                answer.append(cnt)
-                cnt = 0
-                break
-    if cnt > 0: 
+    required = []
+    for i in range(len(progresses)):
+        required.append(math.ceil((100-progresses[i])/speeds[i]))
+    max_days = required[0]
+    for i in required:
+        if i > max_days:
+            answer.append(cnt)
+            cnt = 1
+            max_days = i
+        else:
+            cnt += 1
+    if cnt > 0:
         answer.append(cnt)
     return answer
