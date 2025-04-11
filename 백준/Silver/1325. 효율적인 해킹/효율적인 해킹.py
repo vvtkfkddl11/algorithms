4791a04ea2373b902da8ac8842a0b0b2ee831d1e
Sarang Kim <77392642+vvtkfkddl11@users.noreply.github.com>
2025-04-12 01:56:22 +0900

    [Silver I] Title: 효율적인 해킹, Time: 10584 ms, Memory: 211060 KB -BaekjoonHub

--- 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.py
+++ 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.py
@@ -2,35 +2,41 @@ import sys
 from collections import deque
 
 n, m = map(int, sys.stdin.readline().split())
-graph = [[] for _ in range(n+1)]
-answer = [0]*(n+1)
-max_value = 0
+graph = [[] for i in range(n+1)]  # graph = [] * (n+1) 아님
+visited_cnt = []
+answer = []
+max_v = 0
 
-
-def dfs(v):
-    queue = deque()
-    queue.append(v)
-    visited[v] = 1
-    while queue:
-        now_node = queue.popleft()
-        for i in graph[now_node]:
-            if visited[i] == 0:
-                answer[i] += 1
-                visited[i] = 1
-                queue.append(i)
-
-
-for _ in range(m):  # 간선 수만큼
+for i in range(m):
     a, b = map(int, sys.stdin.readline().split())
-    graph[a].append(b)
+    graph[b] += [a]
+
+
+def bfs(node):
+    global cnt
+    q = deque()
+    q.append(node)
+    visited[node] = 1
+    while q:
+        x = q.popleft()
+        for i in graph[x]:
+            if visited[i] == 0:
+                visited[i] = 1
+                q.append(i)
+                cnt += 1
+    return
+
 
 for i in range(1, n+1):
-    visited = [0]*(n+1)
-    dfs(i)
+    cnt = 0
+    visited = [0] * (n+1)
+    bfs(i)
+    visited_cnt.append(cnt)
 
-for i in range(1, n+1):
-    max_value = max(max_value, answer[i])
+max_v = max(visited_cnt)
 
-for i in range(1, n+1):
-    if answer[i] == max_value:
-        print(i)
+for i in range(len(visited_cnt)):
+    if visited_cnt[i] == max_v:
+        answer.append(i+1)
+
+print(*answer)
