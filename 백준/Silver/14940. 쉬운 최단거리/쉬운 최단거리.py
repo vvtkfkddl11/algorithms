f441a695dd09f9f68d3bc7a44405dacfd54d4056
Sarang Kim <77392642+vvtkfkddl11@users.noreply.github.com>
2025-06-09 07:59:06 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 728 ms, Memory: 41660 KB -BaekjoonHub

--- 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.py
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.py
@@ -10,11 +10,10 @@ start_x, start_y = 0, 0
 for i in range(n):
     for j in range(m):
         if graph[i][j] == 2:
-            start_x = i
-            start_y = j
-
-queue = deque()
-queue.append((start_x, start_y))
+            queue = deque([(i, j)])
+            result[i][j] = 0
+        elif graph[i][j] == 0:
+            result[i][j] = 0
 
 dx = [0, 0, -1, 1]
 dy = [1, -1, 0, 0]
@@ -27,15 +26,6 @@ while queue:
         if 0 <= nx < n and 0 <= ny < m and graph[nx][ny] == 1 and result[nx][ny] == -1:
             result[nx][ny] = result[x][y] + 1
             queue.append((nx, ny))
-
-for i in range(n):
-    for j in range(m):
-        if graph[i][j] == 0 and result[i][j] == -1:
-            result[i][j] = 0
-        elif graph[i][j] != 0 and result[i][j] != -1:
-            result[i][j] += 1
-
-result[start_x][start_y] = 0
 
 for i in result:
     print(*i)
