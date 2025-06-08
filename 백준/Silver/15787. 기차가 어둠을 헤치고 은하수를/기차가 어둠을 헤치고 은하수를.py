e6351fd3247fa08f21ef6aa5d45236a34a8fb467
Sarang Kim <77392642+vvtkfkddl11@users.noreply.github.com>
2025-06-09 06:27:42 +0900

    [Silver II] Title: 기차가 어둠을 헤치고 은하수를, Time: 332 ms, Memory: 55720 KB -BaekjoonHub

--- 백준/Silver/15787. 기차가 어둠을 헤치고 은하수를/기차가 어둠을 헤치고 은하수를.py
+++ 백준/Silver/15787. 기차가 어둠을 헤치고 은하수를/기차가 어둠을 헤치고 은하수를.py
@@ -1,26 +1,36 @@
 import sys
 
 n, m = map(int, sys.stdin.readline().split())
-dic = {i: set() for i in range(1, n+1)}  # 기차의 좌석 상태를 저장할 딕셔너리
+trains = [[0]*20 for _ in range(n)]
 
-for _ in range(m):
-    command = sys.stdin.readline().strip().split()
-    train_idx = int(command[1])
-    
-    if command[0] == '1':  # 좌석에 사람을 태움
-        dic[train_idx].add(int(command[2]))
-    elif command[0] == '2':  # 좌석에서 사람을 내림
-        dic[train_idx].discard(int(command[2]))
-    elif command[0] == '3':  # 모든 승객을 뒤로 한 칸 이동
-        dic[train_idx] = {x + 1 for x in dic[train_idx] if x < 20}
-    elif command[0] == '4':  # 모든 승객을 앞으로 한 칸 이동
-        dic[train_idx] = {x - 1 for x in dic[train_idx] if x > 1}
+def command1(commands):
+    trains[commands[1]-1][commands[2]-1] = 1
 
-# 고유한 기차 상태를 찾기 위한 집합
-unique_trains = set()
+def command2(commands):
+    trains[commands[1]-1][commands[2]-1] = 0
+    return
 
-for i in range(1, n + 1):
-    state = tuple(sorted(dic[i]))  # 집합을 튜플로 변환해 순서 상관없이 비교
-    unique_trains.add(state)
+def command3(commands):
+    for i in range(20-1, 0, -1):
+        trains[commands[1]-1][i] = trains[commands[1]-1][i-1]
+    trains[commands[1]-1][0] = 0
+    return
 
-print(len(unique_trains))
+def command4(commands):
+    for i in range(20-1):
+        trains[commands[1]-1][i] = trains[commands[1]-1][i+1]
+    trains[commands[1]-1][-1] = 0
+    return
+
+for i in range(m):
+    commands = list(map(int, sys.stdin.readline().split()))
+    if commands[0] == 1:
+        command1(commands)
+    elif commands[0] == 2:
+        command2(commands)
+    elif commands[0] == 3:
+        command3(commands)
+    elif commands[0] == 4:
+        command4(commands)
+
+print(len(set(tuple(x) for x in trains)))
