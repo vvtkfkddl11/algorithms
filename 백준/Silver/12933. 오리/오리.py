6fa6fb7717ba687e5f7b5f181f55487e69e94406
Sarang Kim <77392642+vvtkfkddl11@users.noreply.github.com>
2025-06-11 11:59:54 +0900

    [Silver II] Title: 오리, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/12933. 오리/오리.py
+++ 백준/Silver/12933. 오리/오리.py
@@ -1,69 +1,55 @@
 import sys
 
-sounds = str(sys.stdin.readline().strip())
-arr = []
-flag = 0
-answer = 0
+sounds = sys.stdin.readline().strip()
 
-for i in sounds:
-    if i == 'q':
-        if 'quack' in arr:
-            for j in range(len(arr)):
-                if arr[j] == 'quack':
-                    arr[j] = 'q'
-                    break
-        else:
-            arr.append(i)
-    elif i == 'u':
-        if 'q' in arr:
-            for j in range(len(arr)):
-                if arr[j] == 'q':
-                    arr[j] += 'u'
-                    break
-        else:
-            flag = 1
-            break
-    elif i == 'a':
-        if 'qu' in arr:
-            for j in range(len(arr)):
-                if arr[j] == 'qu':
-                    arr[j] += 'a'
-                    break
-        else:
-            flag = 1
-            break
-    elif i == 'c':
-        if 'qua' in arr:
-            for j in range(len(arr)):
-                if arr[j] == 'qua':
-                    arr[j] += 'c'
-                    break
-        else:
-            flag = 1
-            break
-    elif i == 'k':
-        if 'quac' in arr:
-            for j in range(len(arr)):
-                if arr[j] == 'quac':
-                    arr[j] += 'k'
-                    break
-        else:
-            flag = 1
-            break
-    else:
-        flag = 1
-        break
+# 각 상태별 카운터 (O(1) 접근)
+q_count = 0      # "q" 상태
+qu_count = 0     # "qu" 상태
+qua_count = 0    # "qua" 상태
+quac_count = 0   # "quac" 상태
+max_ducks = 0    # 동시에 울었던 최대 오리 수
 
-if flag == 1:
-    print(-1)
-else:
-    for i in arr:
-        if i != 'quack':
+for char in sounds:
+    if char == 'q':
+        q_count += 1
+        current_ducks = q_count + qu_count + qua_count + quac_count
+        max_ducks = max(max_ducks, current_ducks)
+
+    elif char == 'u':
+        if q_count > 0:
+            q_count -= 1
+            qu_count += 1
+        else:
             print(-1)
-            exit(0)
+            exit()
+
+    elif char == 'a':
+        if qu_count > 0:
+            qu_count -= 1
+            qua_count += 1
         else:
-            answer += 1
-    if answer == 0:
-        print(-1)
+            print(-1)
+            exit()
+
+    elif char == 'c':
+        if qua_count > 0:
+            qua_count -= 1
+            quac_count += 1
+        else:
+            print(-1)
+            exit()
+
+    elif char == 'k':
+        if quac_count > 0:
+            quac_count -= 1
+        else:
+            print(-1)
+            exit()
     else:
-        print(answer)
+        print(-1)
+        exit()
+
+if q_count == 0 and qu_count == 0 and qua_count == 0 and quac_count == 0:
+    print(max_ducks)
+else:
+    print(-1)
